cfd762d40ab30891b44b24d81529589c7ef60ccc
Nicholas Gunner <gunnernr@gmail.com>
2016-09-28 17:08:02 -0400

    test

--- js/map.js
+++ js/map.js
@@ -1,37 +1,23 @@
+
+// Get points geojson data //
+var propertyData = (function () {
+    var propertyData = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': pointsInfoApi,
+        'dataType': "json",
+        'success': function (data) {
+            propertyData = data;
+        }
+    });
+    return propertyData;
+})();
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiYnVmZmFsb2J1c2luZXNzZmlyc3QiLCJhIjoiY2l0bmRscXlrMDQyZTJ4bDR3czB4aGplZCJ9.hT3YUWzzRC-s0jGYQU1rXQ';
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/buffalobusinessfirst/citndoi0c003m2iprlhyvg4ov',
     zoom: 12,
-    center: [-122.447303, 37.753574]
-});
-
-map.on('load', function () {
-    map.addSource('properties', {
-        type: 'vector',
-        url: 'data/core2016.geojson'
-    });
-    map.addLayer({
-        'id': 'population',
-        'type': 'circle',
-        'source': 'properties',
-        'source-layer': 'core2016',
-        'paint': {
-            // make circles larger as the user zooms from z12 to z22
-            'circle-radius': {
-                'base': 1.75,
-                'stops': [[12, 2], [22, 180]]
-            },
-            // color circles by ethnicity, using data-driven styles
-            'circle-color': {
-                property: 'status',
-                type: 'categorical',
-                stops: [
-                    ['Completed', '#CA6266'],
-                    ['Under', '#009770'],
-                    ['Z-Proposed', '#53578F'],
-                    ['Completed', '#CA6266']]
-            }
-        }
-    });
+    center: [-79, 42]
 });
