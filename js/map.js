1b2b1999da8cf64347680f27c82e5d7fb5963ebe
Nicholas Gunner <gunnernr@gmail.com>
2016-10-01 16:48:26 -0400

    again

--- js/map.js
+++ js/map.js
@@ -95,8 +95,8 @@ map.on('load', function () {
       "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
       "text-size": {
         "stops": [
-          [15, 0],
-          [18, 12]
+          [18, 0],
+          [20, 12]
         ]
       },
       "text-offset": [0, 0.5],
