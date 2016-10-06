2fd11930ad21952760c9cdbc966985181073f20b
Nicholas Gunner <gunnernr@gmail.com>
2016-10-06 12:11:44 -0400

    added markers to list view

--- js/maplist.js
+++ js/maplist.js
@@ -4,9 +4,9 @@ function renderMapList() {
   for (var i = 0; i < orbitistPointsGeojsonCleaned.features.length; i++) {
     var feature = orbitistPointsGeojsonCleaned.features[i];
     if (numberedPoints == "true"){
-      var  listItem  = '<hr><div class="map-list-item" id="' + feature.properties.point_id + '"><img src="' + feature.properties.point_thumbnail + '" /><p>' + feature.properties.point_position_number + " - " + feature.properties.point_title + '</p></div><script>document.getElementById("' + feature.properties.point_id + '").addEventListener("click", function () {map.flyTo({center:[' + feature.geometry.coordinates + '],zoom: 18,bearing: 90 * (.5 - Math.random()),pitch: 60});});</script>';
+      var  listItem  = '<hr><div class="map-list-item" id="' + feature.properties.point_id + '"><img src="' + feature.properties.point_marker_url + '" /><p>' + feature.properties.point_position_number + " - " + feature.properties.point_title + '</p></div><script>document.getElementById("' + feature.properties.point_id + '").addEventListener("click", function () {map.flyTo({center:[' + feature.geometry.coordinates + '],zoom: 18,bearing: 90 * (.5 - Math.random()),pitch: 60});});</script>';
     } else {
-      var  listItem  = '<hr><div class="map-list-item" id="' + feature.properties.point_id + '"><img src="' + feature.properties.point_thumbnail + '" /><p>' + feature.properties.point_title + '</p></div><script>document.getElementById("' + feature.properties.point_id + '").addEventListener("click", function () {map.flyTo({center:[' + feature.geometry.coordinates + '],zoom: 18,bearing: 90 * (.5 - Math.random()),pitch: 60});});</script>';
+      var  listItem  = '<hr><div class="map-list-item" id="' + feature.properties.point_id + '"><img src="' + feature.properties.point_marker_url + '" /><p>' + feature.properties.point_title + '</p></div><script>document.getElementById("' + feature.properties.point_id + '").addEventListener("click", function () {map.flyTo({center:[' + feature.geometry.coordinates + '],zoom: 18,bearing: 90 * (.5 - Math.random()),pitch: 60});});</script>';
     }
     $('div.map-list').append(listItem);
   }
