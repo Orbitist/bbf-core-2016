522c541b58494b374b9502f7a108a3bd609b3144
Nicholas Gunner <gunnernr@gmail.com>
2016-10-06 11:44:38 -0400

    more custom stuff

--- js/controls.js
+++ js/controls.js
@@ -55,9 +55,7 @@ jQuery(document).ready(function () {
 jQuery(document).ready(function () {
   if (jQuery(window).width() >= 800){
     jQuery('.map-info-toggle, .map-help').click(function () {
-      jQuery('.map').css({"left": "0","width":"calc(100% - 500px)"}),
       jQuery('.map-info').css({"left": "calc(100% - 500px)"}),
-      jQuery('.map-list').css({"left": "-300px"}),
       jQuery('.map-toggle-info').css({"left": "calc(100% - 490px)"}),
       jQuery('.map-toggle-list').css({"right": "calc(100% + 10px)"}),
       jQuery('.map-list-toggle').css({"left": "10px"})
@@ -71,7 +69,6 @@ jQuery(document).ready(function () {
       jQuery('.mapboxgl-popup').css({"top": "-100%"})
     });
     jQuery('.map-toggle-info').click(function () {
-      jQuery('.map').css({"left": "0","width": "100%"}),
       jQuery('.map-info').css({"left": "100%"}),
       jQuery('.map-toggle-info').css({"left": "calc(100% + 10px)"})
     });
